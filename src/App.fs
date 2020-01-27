0bd4404e59f508433c60f24f7489c8c4159a2a8c
Niklas <noresjo@gmail.com>
2020-01-27 23:10:46 +0100

    Fix hot reload

--- src/App.fs
+++ src/App.fs
@@ -4,8 +4,15 @@ open Browser.Dom
 open System
 open Fable.Pixi
 let PIXI = PIXI.pixi
+open Fable.Core.JsInterop
 
-let app = PIXI.Application.Create()
+let PIXI = Fable.Pixi.PIXI.pixi
+
+let options  = jsOptions<Fable.Pixi.PIXI.ApplicationStaticOptions>(fun x ->
+    x.antialias <- Some(true)
+    )
+
+let app = PIXI.Application.Create(options)
 document.body.appendChild app.view |> ignore
 
 let img = PIXI.Sprite.from "./fable.ico"
@@ -35,3 +42,28 @@ let hex =
   result
   
 app.stage.addChild hex|> ignore
+let graphics = PIXI.Graphics.Create().beginFill((float)0xDE3249).drawRect(0., 0., 100., 100.).endFill()
+let graphics2 = PIXI.Graphics.Create().lineStyle(4.0, (float)0xFFBD01, 1.).beginFill((float)0xC34288).drawRect(0., 0., 100., 100.).endFill();
+
+graphics.x <- 100.
+graphics2.x <- 400.
+graphics.y <- 350.
+graphics2.y <- 200.
+
+
+let gfx = [
+    graphics
+    graphics2
+]
+
+gfx 
+|> List.map ((fun (x) -> x.pivot.x <- 50.;x.pivot.y <- 50.; x ) >> app.stage.addChild) 
+|> ignore
+
+let update(_) = 
+    gfx 
+    |> List.mapi (fun i (x : Fable.Pixi.PIXI.Graphics) -> x.angle <- x.angle + ((float)i+1.)*1.) |> ignore
+    graphics2.y <- System.Math.Sin(graphics2.rotation) * 100. + 200.
+    None
+
+app.ticker.add update |> ignore
