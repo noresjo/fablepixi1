3b10cdee5a1f129009607340ffbc70cf33097982
Niklas <noresjo@gmail.com>
2020-01-28 23:13:19 +0100

    A hex

--- src/App.fs
+++ src/App.fs
@@ -1,10 +1,37 @@
 module AmazingApp
 
 open Browser.Dom
-let PIXI = Fable.Pixi.PIXI.pixi
+open System
+open Fable.Pixi
+let PIXI = PIXI.pixi
 
 let app = PIXI.Application.Create()
 document.body.appendChild app.view |> ignore
 
 let img = PIXI.Sprite.from "./fable.ico"
 app.stage.addChild img |> ignore
+
+let flatHexVertex (centerPoint : PIXI.Point) size i =
+    let angleDegrees = 60 * i
+    let angleRadians = Math.PI * float angleDegrees / 180.
+    let x = centerPoint.x + size * cos angleRadians
+    let y = centerPoint.y + size * sin angleRadians
+    PIXI.Point.Create(x, y)
+
+let flatUnitHexagon size =
+    [0..5] |> List.map (flatHexVertex (PIXI.Point.Create(0., 0.)) size)
+
+
+let createFlatHexagonGraphics size =
+  PIXI.Graphics
+    .Create()
+    .lineStyle(color = (float)0x564534, width = 10.)
+    .drawPolygon(Fable.Core.U3.Case2 (ResizeArray<PIXI.Point> (flatUnitHexagon size)))
+
+let hex = 
+  let result = createFlatHexagonGraphics 30.
+  result.x <- 30.
+  result.y <- 30.
+  result
+  
+app.stage.addChild hex|> ignore
